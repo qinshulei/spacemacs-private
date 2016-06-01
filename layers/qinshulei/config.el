e7e84261270cdcd812bfca55b40e266fc846377b
qinshulei <527072230@qq.com>
2016-06-01 09:59:21 +0800

    remove with-eval-after-load for keybinding.
    more information : https://emacs-china.org/t/spacemacs/476/2

--- layers/qinshulei/config.el
+++ layers/qinshulei/config.el
@@ -1,10 +1,9 @@
 (defun qinshulei/post-init-expand-region ()
-  (with-eval-after-load 'expand-region
-    (global-set-key (kbd "C-=") 'er/expand-region)))
+  (global-set-key (kbd "C-=") 'er/expand-region)
+  )
 
 (defun qinshulei/post-init-avy ()
-  (with-eval-after-load 'avy
-    (global-set-key (kbd "C-;") 'avy-goto-word-or-subword-1)))
+  (global-set-key (kbd "C-;") 'avy-goto-word-or-subword-1))
 
 
 (global-set-key (kbd "C-c j") 'join-line)
