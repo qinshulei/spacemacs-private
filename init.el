bbecda36da2264a7bb0a391a3f2bff7742c9e8d5
qinshulei <527072230@qq.com>
2017-07-07 12:13:16 +0800

    support lua

--- init.el
+++ init.el
@@ -79,6 +79,7 @@ values."
                                        shell-scripts
                                        yaml
                                        graphviz
+                                       lua
                                        )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
