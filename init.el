fcd32fc91db7f216381fb59d5e4eb332167f9bd7
qinshulei <527072230@qq.com>
2018-02-25 11:23:12 +0800

    add spell check and syntax check

--- init.el
+++ init.el
@@ -81,6 +81,8 @@ values."
                                        graphviz
                                        lua
                                        go
+                                       spell-checking
+                                       syntax-checking
                                        )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
