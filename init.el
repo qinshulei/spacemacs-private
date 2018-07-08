51b196e444e057c90b654ab625f52ba503853c81
Qin Shulei <527072230@qq.com>
2018-07-08 22:46:29 +0800

    Update init.el

--- init.el
+++ init.el
@@ -111,6 +111,7 @@ values."
                                       nginx-mode
                                       git-blamed
                                       alda-mode
+                                      selectric-mode
                                       )
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '(
